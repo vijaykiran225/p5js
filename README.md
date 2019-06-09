a1ac04ce9d0dd2acefd4e0c431e6480d8a2055bd
Vijay Kiran <vijaykiran225@gmail.com>
2019-06-09 16:58:45 +0530

    indendation and readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 visualizing math with p5
 
  - times tables
-    - multiply number with value and connect that value on a point on circle
+    - multiply number with value and connect that value on a point on circle. circle is divided into many equal parts (configured).
--- p5/times_tables/sketch.js
+++ p5/times_tables/sketch.js
@@ -7,27 +7,20 @@ var multiplierAmount = 20;
 function setup() {
     // put setup code here
     createCanvas(400, 400);
-    slider=createSlider(1,500,1);
+    slider = createSlider(1, 500, 1);
 }
-
 
 function draw() {
     // put drawing code here
-  	clear();
-  	// background(0);
-
-	parts=slider.value();
-
-	textSize(10);
-	// fill(255);
-    text("Current Value is "+parts, 10,10);
-
-    text("Multipler Value is "+multiplierAmount, 20,20);
-
+    clear();
+    // background(0);
+    parts = slider.value();
+    textSize(10);
+    // fill(255);
+    text("Current Value is " + parts, 10, 10);
+    text("Multipler Value is " + multiplierAmount, 10, 20);
     translate(width / 2, height / 2);
-  
-  	var vjPoints=[];
-
+    var vjPoints = [];
     for (var i = 0; i < parts; i++) {
         eachPartAngle = eachPartAngle + (TWO_PI / parts);
         var x = radius * cos(eachPartAngle);
@@ -36,22 +29,18 @@ function draw() {
         strokeWeight(4);
         point(x, y);
         var vjPoint = {
-        	posX : x,
-        	posY : y
+            posX: x,
+            posY: y
         };
-        vjPoints[i]=vjPoint;
-
+        vjPoints[i] = vjPoint;
     }
     // print(vjPoints)
-
     for (var i = 0; i < vjPoints.length; i++) {
-    	var targetId = (i * multiplierAmount ) % parts;
-    	stroke(255,0, 0);
+        var targetId = (i * multiplierAmount) % parts;
+        stroke(255, 0, 0);
         strokeWeight(1);
-    	var curr=vjPoints[i];
-    	var targetPoint=vjPoints[targetId];
-    	line(curr.posX,curr.posY,
-    		targetPoint.posX,targetPoint.posY);
+        var curr = vjPoints[i];
+        var targetPoint = vjPoints[targetId];
+        line(curr.posX, curr.posY, targetPoint.posX, targetPoint.posY);
     }
-
 }
